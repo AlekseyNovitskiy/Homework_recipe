c975d66596e7af46f13d48593a283ab26e5787ca
AlekseyNovitskiy <novickii_aleksei@mail.ru>
2022-12-06 11:51:29 +0300

    Initial commit

---
+++ src/main/java/com/example/homework_recipe/HomeworkRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework_recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkRecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkRecipeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework_recipe/HomeworkRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework_recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
